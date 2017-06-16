e059feb60fcf87fe7d4e1884c8b13e7d1e215d50
Arete Code <aretecode@gmail.com>
2017-06-16 02:07:21 -0700

    evaluate, debugging, improve regex, interpolate
    - 🐴 evaluate conditionals,
    - 🔊 debugging,
    - 🎯better regex
    - 🎁 interpolation
    - 👕 lint autofix

--- typed/Conditional.js
+++ typed/Conditional.js
@@ -1,4 +1,4 @@
-const { ChainedMap } = require('chain-able')
+const {ChainedMap} = require('chain-able')
 
 class Conditional extends ChainedMap {
   name(name: string): Conditional {
@@ -13,22 +13,46 @@ class Conditional extends ChainedMap {
     return this.parent !== null
   }
 
+  // var conditions = {_global: true, _window: false}
+  // var expression = `return ` + '_global && _window'
+  // var keys = Object.keys(conditions)
+  // var fn = new Function(keys, expression)
+  // fn.apply(Object.values(conditions))
   /**
    * @desc get root parent,
    *       then split & check each name to ensure enabled,
    *       ...dun
    * @return {boolean}
    */
-  isEnabled(): boolean {
+  isEnabled() {
     let parent = this.parent
     while (parent && parent.parent) {
       parent = parent.parent
     }
 
+    var conditions = parent.get('conditions')
+    var debug = parent.get('debug')
+
     const namespace = this.get('name').split('.')
+    if (debug) {
+      console.log(`parts: `, namespace)
+    }
+
     const enabled =
-      namespace.filter(name => parent.get('conditions')[name]).length ===
-      namespace.length
+      namespace.filter(name => {
+        var expression = `return ` + (conditions[name] || name)
+        var keys = Object.keys(conditions)
+        var fn = new Function(keys, expression)
+        const evaluation = fn.apply(undefined, Object.values(conditions))
+
+        if (debug) {
+          console.log(expression.replace('return', '') + ' ==', !!evaluation)
+          console.log('\n')
+        }
+
+        if (evaluation) return true
+        return false
+      }).length === namespace.length
 
     return enabled
   }
@@ -37,4 +61,4 @@ class Conditional extends ChainedMap {
 Conditional.Conditional = Conditional
 module.exports = Conditional
 module.exports.default = module.exports
-Object.defineProperty(module.exports, '__esModule', { value: true })
+Object.defineProperty(module.exports, '__esModule', {value: true})
